d50fc2d2335c10774ae1d0a996229445a03b9466
daudonhere <hellodadedaud#gmail.com>
2024-06-24 02:52:37 +0700

    configure redux for contact endpoint

--- src/redux/actions/contactAction.js
+++ src/redux/actions/contactAction.js
@@ -15,3 +15,51 @@ export const getAllContact = () => {
         }
     };
 };
+
+export const SAVE_CONTACT_REQUEST = 'SAVE_CONTACT_REQUEST';
+export const SAVE_CONTACT_SUCCESS = 'SAVE_CONTACT_SUCCESS';
+export const SAVE_CONTACT_FAILURE = 'SAVE_CONTACT_FAILURE';
+
+export const saveContact = (data) => {
+    return async (dispatch) => {
+        dispatch({ type: SAVE_CONTACT_REQUEST });
+        try {
+            const response = await axios.post(`${process.env.API_URL}/contact`, data);
+            dispatch({ type: SAVE_CONTACT_SUCCESS, payload: response.data });
+        } catch (error) {
+            dispatch({ type: SAVE_CONTACT_FAILURE, payload: error.message });
+        }
+    };
+};
+
+export const GET_CONTACT_REQUEST = 'GET_CONTACT_REQUEST';
+export const GET_CONTACT_SUCCESS = 'GET_CONTACT_SUCCESS';
+export const GET_CONTACT_FAILURE = 'GET_CONTACT_FAILURE';
+
+export const getContactId = (id) => {
+    return async (dispatch) => {
+        dispatch({ type: GET_CONTACT_REQUEST });
+        try {
+            const response = await axios.get(`${process.env.API_URL}/contact`, id);
+            dispatch({ type: GET_CONTACT_SUCCESS, payload: response.data });
+        } catch (error) {
+            dispatch({ type: GET_CONTACT_FAILURE, payload: error.message });
+        }
+    };
+};
+
+export const EDIT_CONTACT_REQUEST = 'EDIT_CONTACT_REQUEST';
+export const EDIT_CONTACT_SUCCESS = 'EDIT_CONTACT_SUCCESS';
+export const EDIT_CONTACT_FAILURE = 'EDIT_CONTACT_FAILURE';
+
+export const editContact = (id) => {
+    return async (dispatch) => {
+        dispatch({ type: EDIT_CONTACT_REQUEST });
+        try {
+            const response = await axios.get(`${process.env.API_URL}/contact`, id);
+            dispatch({ type: EDIT_CONTACT_SUCCESS, payload: response.data });
+        } catch (error) {
+            dispatch({ type: EDIT_CONTACT_FAILURE, payload: error.message });
+        }
+    };
+};
--- src/redux/reducers/contactReducer.js
+++ src/redux/reducers/contactReducer.js
@@ -1,4 +1,17 @@
-import { FETCH_CONTACT_REQUEST, FETCH_CONTACT_SUCCESS, FETCH_CONTACT_FAILURE } from '../actions/contactAction';
+import {
+    FETCH_CONTACT_REQUEST,
+    FETCH_CONTACT_SUCCESS,
+    FETCH_CONTACT_FAILURE,
+    SAVE_CONTACT_REQUEST,
+    SAVE_CONTACT_SUCCESS,
+    SAVE_CONTACT_FAILURE,
+    GET_CONTACT_REQUEST,
+    GET_CONTACT_SUCCESS,
+    GET_CONTACT_FAILURE,
+    EDIT_CONTACT_REQUEST,
+    EDIT_CONTACT_SUCCESS,
+    EDIT_CONTACT_FAILURE
+} from '../actions/contactAction';
 
 const initialState = {
     loading: false,
@@ -20,6 +33,57 @@ const contactReducer = (state = initialState, action) => {
                 error: '',
             };
         case FETCH_CONTACT_FAILURE:
+            return {
+                loading: false,
+                result: {},
+                error: action.payload,
+            };
+        case SAVE_CONTACT_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            };
+        case SAVE_CONTACT_SUCCESS:
+            return {
+                loading: false,
+                result: action.payload,
+                error: '',
+            };
+        case SAVE_CONTACT_FAILURE:
+            return {
+                loading: false,
+                result: {},
+                error: action.payload,
+            };
+        case GET_CONTACT_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            };
+        case GET_CONTACT_SUCCESS:
+            return {
+                loading: false,
+                result: action.payload,
+                error: '',
+            };
+        case GET_CONTACT_FAILURE:
+            return {
+                loading: false,
+                result: {},
+                error: action.payload,
+            };
+        case EDIT_CONTACT_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            };
+        case EDIT_CONTACT_SUCCESS:
+            return {
+                loading: false,
+                result: action.payload,
+                error: '',
+            };
+        case EDIT_CONTACT_FAILURE:
             return {
                 loading: false,
                 result: {},
